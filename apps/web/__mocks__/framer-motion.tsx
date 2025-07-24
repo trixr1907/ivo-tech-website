a5fe4bb8ff1210540070ff462ff20e8f3b0c2168
IVO-TECH <admin@ivo-tech.com>
2025-07-24 05:02:23 +0200

    chore: Update framer-motion mock

--- apps/web/__mocks__/framer-motion.tsx
+++ apps/web/__mocks__/framer-motion.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
 
-// Mock the motion components to render as regular HTML elements
 const createMockComponent = (element: string) => 
   React.forwardRef((props: any, ref) => {
-    const { children, ...rest } = props;
+    const { children, initial, animate, whileHover, whileInView, viewport, transition, whileTap, ...rest } = props;
     return React.createElement(element, { ...rest, ref }, children);
   });
 
-export const motion = {
-  div: createMockComponent('div'),
-  a: createMockComponent('a'),
-  p: createMockComponent('p'),
-  h1: createMockComponent('h1'),
-  h2: createMockComponent('h2'),
-  h3: createMockComponent('h3'),
-  span: createMockComponent('span'),
-  img: createMockComponent('img'),
-  button: createMockComponent('button'),
-  section: createMockComponent('section'),
-  h3: createMockComponent('h3'),
-  ul: createMockComponent('ul'),
-  li: createMockComponent('li'),
+const handler = {
+  get: function(target: any, prop: string) {
+    return createMockComponent(prop);
+  }
 };
+
+export const motion = new Proxy({}, handler);
