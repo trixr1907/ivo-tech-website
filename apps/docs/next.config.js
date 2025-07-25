c490d6693e85cc27449f613224ff5759dddda7bf
Ivo <trixr@hotmail.de>
2025-07-25 04:38:14 +0200

    fix: update nextjs configs for proper vercel deployment

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['@repo/ui'],
   output: 'standalone',
-  experimental: {
-    typedRoutes: true,
-  },
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
 };
